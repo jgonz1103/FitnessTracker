9416eb0b2f892d9670ff35299329bcacb9c45f7b
Joe <joe_gonzalezjr@yahoo.com>
2022-11-09 14:03:30 -0500

    users.js routines.js

--- db/activities.js
+++ db/activities.js
@@ -1,31 +1,115 @@
-const client = require("./client")
+const client = require("./client");
+const { addActivityToRoutine } = require("./routine_activities");
 
 // database functions
 async function getAllActivities() {
-
+  try {
+    const {rows: activityId} = await client.query(`
+    SELECT id
+    FROM activities
+    `);
+    const posts = await Promise.all(activityId.map(
+        post=> getActivityById (post.id)
+    ));
+    return activities;
+} catch (error) {
+    throw error;
+}
 }
 
 async function getActivityById(id) {
-  
+  try {
+    const { rows: activityIds } = await client.query(`
+    SELECT id
+    FROM activities
+    WHERE "authorId"=${userId};
+  `);
+  const activities = await Promise.all(activityIds.map(
+    activity => getActivityById(activity.id)
+  ));
+    return activities;
+} catch (error) {
+    throw error;
+}
 }
 
 async function getActivityByName(name) {
+  try {
+    const { rows: activityIds } = await client.query(`
+      SELECT activities.id
+      FROM activities
+      JOIN  routine_activities ON activities.id=routine_activities."activityId"
+      JOIN activities ON activities.id=routine_activities."activityId"
+      WHERE activities.name=$1;
+    `, [name]);
 
+    return await Promise.all(activityIds.map(
+      activity => getActivityById(activity.id)
+    ));
+  } catch (error) {
+    throw error;
+  }
 }
 
 // select and return an array of all activities
 async function attachActivitiesToRoutines(routines) {
+  try {
+    const createActivityRoutinePromises = routineActivies.map((routines) => {
+        return createActivitiesRoutines(activityId, routine.id);
+    });
+    await Promise.all(createActivityRoutinePromises);
+} catch (error) {
+    throw error;
+}
 }
 
 // return the new activity
 async function createActivity({ name, description }) {
-
+  try {
+    const {
+        rows: [activities],
+    } = await client.query(
+        `
+    INSERT INTO activities("activityId", name, description)
+    VALUES ($1, $2, $3)
+    RETURNING *;
+    `,
+        [activityId, name, description]
+    );
+    const routineActivities = await createActivities(routines);
+    return await addActivityToRoutine(activities.id, routineActivies);
+} catch (error) {
+    throw error;
+}
 }
 
 // don't try to update the id
 // do update the name and description
 // return the updated activity
 async function updateActivity({ id, ...fields }) {
+  const setString = Object.keys(fields)
+  .map((key, index) => `"${key}"=$${index + 1}`)
+  .join(", ");
+
+if (setString.length === 0) return;
+
+try {
+  const {
+      rows: [activity],
+  } = await client.query(
+      `
+      UPDATE Activity
+      SET ${setString}
+      WHERE id=${id}
+      RETURNING *;
+  `,
+      Object.values(fields)
+  );
+
+  return user;
+} catch (error) {
+  throw error;
+}
 
 }
 
--- db/routines.js
+++ db/routines.js
@@ -1,6 +1,33 @@
 const client = require('./client');
 
 async function getRoutineById(id){
+  try {
+    const {rows: [routine]} = await client.query(`
+    SELECT *
+    FROM routines
+    WHERE id=$1;
+    `, [routinesId]);
+
+    if (!routine){
+        throw {
+            name:'routineNotFound', 
+            message:'There is no routine with that ID'
+        }
+    }
+
+    const {rows: routines} = await client.query(`
+    SELECT routine.*
+    FROM routines
+    JOIN activities_routine ON routine.id=activities_routine."routinesId"
+    WHERE post_tags."postId"=$1
+    `, [postId])
+
+    activities.routine = routines;
+
+    return routines;
+    } catch (error) {
+        throw error;   
+    }
 }
 
 async function getRoutinesWithoutActivities(){
